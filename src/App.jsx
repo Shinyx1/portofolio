1a56220a65be38c2318643b86c07086c8581e948
Shinyx1 <jyaqdhan12@gmail.com>
2025-11-06 23:57:06 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -90,11 +90,11 @@ function App() {
             />
             <div className="flex items-center sm:gap-4 gap-2">
               <a 
-                href="./assets/CV.pdf" 
-                download="Javas_Muhammad_Yaqdhan_CV.pdf" 
+                href="./assets/Porto.pdf" 
+                download="Javas_Muhammad_Yaqdhan_Porto.pdf" 
                 className="font-semibold bg-[#1a1a1a] p-4 px-6 rounded-full border border-gray-700 hover:bg-[#222] transition-colors"
               >
-                <ShinyText text="Download CV" disabled={false} speed={3} className="custom-class" />
+                <ShinyText text="Download Porto" disabled={false} speed={3} className="custom-class" />
               </a>
 
               <a href="#project" className="font-semibold bg-[#1a1a1a] p-4 px-6 rounded-full border border-gray-700 hover:bg-[#222] transition-colors">
